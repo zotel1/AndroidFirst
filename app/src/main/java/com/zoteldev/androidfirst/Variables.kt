6765baa26c98ac94d2ccd0ce230b0b73e9bebbf1
zotel1 <zotelsigel@gmail.com>
2024-09-25 17:13:49 -0300

    Variables en kotlin

--- app/src/main/java/com/zoteldev/androidfirst/Variables.kt
+++ app/src/main/java/com/zoteldev/androidfirst/Variables.kt
@@ -1,5 +1,52 @@
 package com.zoteldev.androidfirst
 
+/***
+ * Variables Númericas
+ * */
+
+// Int -2,123,456,788 a 2,123,456,788
+val age:Int = 28;
+
+// Long  -9,223,372,036,854,775,807 a 9,223,372,036,854,775,807
+val example:Long = 28;
+
+// Float
+val floatExample:Float = 30.5f
+
+// Double
+val doubleExample:Double = 3241.3123123
+
+/**
+ * Variables Alfanuméricas*/
+
+
+// Char
+
+val charExample1:Char = 'e';
+
+val charExample2:Char = '1';
+
+val charExample3:Char = '!';
+
+// String
+
+val stringExample:String = "Primeros pasos en android";
+
+/*
+* Variables booleanas*/
+
+val booleanExample1:Boolean = true;
+
+val booleanExample2:Boolean = false;
+
+val booleanExample3:Boolean = true;
+
+
 fun main() {
-    print("Buenas")
+    print("Buenas estos son mis"+ " " + stringExample);
+
+    //val name = "Cristian";
+    //print("Hola me llamo" + " " + name);
+
+
 }
