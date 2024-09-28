aae312f8dc116d95559328338411fcc14322393e
zotel1 <zotelsigel@gmail.com>
2024-09-28 16:36:19 -0300

    seguimos con condicionales

--- app/src/main/java/com/zoteldev/androidfirst/IfElse.kt
+++ app/src/main/java/com/zoteldev/androidfirst/IfElse.kt
@@ -3,8 +3,15 @@ package com.zoteldev.androidfirst
 
 fun ifBasico() {
     val name = "Cris";
+
+    if (name == "Zotel") {
+        println("El nombre es Zotel")
+    } else {
+        println("El usuario registrado no es Cris")
+    }
 }
 
 fun main() {
 
+    ifBasico();
 }
