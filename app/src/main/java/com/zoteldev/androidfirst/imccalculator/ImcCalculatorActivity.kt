0efac3c2b29b74f5253a568a4693474449e40719
zotel1 <zotelsigel@gmail.com>
2024-10-07 18:30:46 -0300

    creamos los metodos viewMale and viewFemale

--- app/src/main/java/com/zoteldev/androidfirst/imccalculator/ImcCalculatorActivity.kt
+++ app/src/main/java/com/zoteldev/androidfirst/imccalculator/ImcCalculatorActivity.kt
@@ -3,11 +3,17 @@ package com.zoteldev.androidfirst.imccalculator
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.zoteldev.androidfirst.R
 
 class ImcCalculatorActivity : AppCompatActivity() {
+
+    private lateint var viewMale:CardView
+
+    private lateint var viewFemale: CardView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,5 +24,10 @@ class ImcCalculatorActivity : AppCompatActivity() {
             insets
         }
 
+    }
+
+    private fun initComponents() {
+        viewMale = findViewById(R.id.viewMale)
+        viewFemale = findViewById(R.id.viewFemale)
     }
 }
