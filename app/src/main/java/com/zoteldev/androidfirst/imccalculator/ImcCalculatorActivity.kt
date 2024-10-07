034e3193ac7539e3703d8c7eaa1a2599dcc939cb
zotel1 <zotelsigel@gmail.com>
2024-10-07 18:49:29 -0300

    cinco horas, cinco minutos

--- app/src/main/java/com/zoteldev/androidfirst/imccalculator/ImcCalculatorActivity.kt
+++ app/src/main/java/com/zoteldev/androidfirst/imccalculator/ImcCalculatorActivity.kt
@@ -10,14 +10,20 @@ import com.zoteldev.androidfirst.R
 
 class ImcCalculatorActivity : AppCompatActivity() {
 
-    private lateint var viewMale:CardView
+    private lateinit var viewMale: CardView
 
-    private lateint var viewFemale: CardView
+    private lateinit var viewFemale: CardView
+
+    private var isMaleSelected: Boolean = true
+
+    private var isFemaleSelected: Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_imc_calculator)
+        initComponents()
+        initListeners()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -29,5 +35,15 @@ class ImcCalculatorActivity : AppCompatActivity() {
     private fun initComponents() {
         viewMale = findViewById(R.id.viewMale)
         viewFemale = findViewById(R.id.viewFemale)
+    }
+
+    private fun initListeners() {
+        viewMale.setOnClickListener { setGenderColor(isMaleSelected) }
+        viewFemale.setOnClickListener { setGenderColor(isFemaleSelected) }
+    }
+
+    private fun setGenderColor(isViewSelected:Boolean) {
+
+        viewMale.setCardBackgroundColor()
     }
 }
