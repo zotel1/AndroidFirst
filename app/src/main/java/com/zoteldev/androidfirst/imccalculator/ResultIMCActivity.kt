959501bcafe03e77d385810a16fefc4e1ec3fb1e
zotel1 <zotelsigel@gmail.com>
2024-10-16 02:54:30 -0300

    segunda aplicacion completa, falta agregar diferentes valores si el usuario es hombre o mujer

--- app/src/main/java/com/zoteldev/androidfirst/imccalculator/ResultIMCActivity.kt
+++ app/src/main/java/com/zoteldev/androidfirst/imccalculator/ResultIMCActivity.kt
@@ -1,9 +1,11 @@
 package com.zoteldev.androidfirst.imccalculator
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.zoteldev.androidfirst.R
@@ -14,6 +16,7 @@ class ResultIMCActivity : AppCompatActivity() {
     private lateinit var tvResult:TextView
     private lateinit var tvIMC:TextView
     private lateinit var tvDescription:TextView
+    private lateinit var btnRecalculate:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,13 +32,14 @@ class ResultIMCActivity : AppCompatActivity() {
 
         initComponents()
         initUI(result)
-
+        initListeners()
     }
 
     private fun initComponents() {
         tvResult = findViewById(R.id.tvResult)
         tvIMC = findViewById(R.id.tvIMC)
         tvDescription = findViewById(R.id.tvDescription)
+        btnRecalculate = findViewById(R.id.btnRecalculate)
     }
 
     private fun initUI(result:Double) {
@@ -44,28 +48,38 @@ class ResultIMCActivity : AppCompatActivity() {
 
         when(result) {
             in 0.00..18.50 -> { // Bajo peso
-                tvResult.text
-                tvDescription.text
+                tvResult.text = getString(R.string.title_peso_bajo)
+                tvResult.setTextColor(ContextCompat.getColor(this, R.color.peso_bajo))
+                tvDescription.text = getString(R.string.description_peso_bajo)
             }
             in 18.51..24.99 -> { // Peso normal
-                tvResult.text
-                tvDescription.text
+                tvResult.text = getString(R.string.title_peso_normal)
+                tvResult.setTextColor(ContextCompat.getColor(this, R.color.peso_normal))
+                tvDescription.text = getString(R.string.description_peso_normal)
             }
             in 25.00..29.99 -> { // Sobrepeso
-                tvResult.text
-                tvDescription.text
+                tvResult.text = getString(R.string.title_sobrepeso)
+                tvResult.setTextColor(ContextCompat.getColor(this, R.color.sobrepeso))
+                tvDescription.text = getString(R.string.description_sobrepeso)
             }
             in 30.00..99.00 -> { // Obesidad
-                tvResult.text
-                tvDescription
+                tvResult.text = getString(R.string.title_obesidad)
+                tvResult.setTextColor(ContextCompat.getColor(this, R.color.obesidad))
+                tvDescription.text = getString(R.string.description_obesidad)
             }
             else -> { // Error
                 tvResult.text = getString(R.string.error)
+
+                tvResult.setTextColor(ContextCompat.getColor(this, R.color.obesidad))
                 tvIMC.text = getString(R.string.error)
                 tvDescription.text = getString(R.string.error)
 
              }
         }
+    }
+
+    private fun initListeners() {
+        btnRecalculate.setOnClickListener { onBackPressed() }
     }
 
 }
