ac8e61413d988b92a670204da797fb8388c983f4
zotel1 <zotelsigel@gmail.com>
2024-10-20 23:11:46 -0300

    update

--- app/src/main/java/com/zoteldev/androidfirst/todoapp/CategorysAdapter.kt
+++ app/src/main/java/com/zoteldev/androidfirst/todoapp/CategorysAdapter.kt
@@ -1,5 +1,20 @@
 package com.zoteldev.androidfirst.todoapp
 
-class CategorysAdapter(private val categories:List<TaskCategory>) {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class CategorysAdapter(private val categories: List<TaskCategory>) :
+    RecyclerView.Adapter<CategoriesViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoriesViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: CategoriesViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 
 }
