449c5b88af80968fc501ba5d030641f117a7939f
zotel1 <zotelsigel@gmail.com>
2024-10-20 23:35:58 -0300

    update, add layout, item_task_category

--- app/src/main/java/com/zoteldev/androidfirst/todoapp/CategorysAdapter.kt
+++ app/src/main/java/com/zoteldev/androidfirst/todoapp/CategorysAdapter.kt
@@ -5,13 +5,12 @@ import androidx.recyclerview.widget.RecyclerView
 
 class CategorysAdapter(private val categories: List<TaskCategory>) :
     RecyclerView.Adapter<CategoriesViewHolder>() {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoriesViewHolder {
         TODO("Not yet implemented")
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount() = categories.size
 
     override fun onBindViewHolder(holder: CategoriesViewHolder, position: Int) {
         TODO("Not yet implemented")
