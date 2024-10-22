2933eb21647aee23b7efee628263864a8375b957
zotel1 <zotelsigel@gmail.com>
2024-10-22 14:39:30 -0300

    update, definimos nuestra listOf de categories y despues lo llamamos en initUi para mostrar nuestra lista de categorias

--- app/src/main/java/com/zoteldev/androidfirst/todoapp/TodoActivity.kt
+++ app/src/main/java/com/zoteldev/androidfirst/todoapp/TodoActivity.kt
@@ -5,12 +5,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.zoteldev.androidfirst.R
 
 class TodoActivity : AppCompatActivity() {
 
+    private val categories = listOf(
+        TaskCategory.Personal,
+        TaskCategory.Business,
+        TaskCategory.Other
+    )
+
     private lateinit var rvCategories: RecyclerView
+    private lateinit var categoriesAdapter: CategoriesAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +41,9 @@ class TodoActivity : AppCompatActivity() {
 
     private fun initUI() {
 
+        categoriesAdapter = CategoriesAdapter(categories)
+        rvCategories.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        rvCategories.adapter = categoriesAdapter
     }
 
 }
