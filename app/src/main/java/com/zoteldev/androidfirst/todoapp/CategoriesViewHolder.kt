40ddbfcc8498778ef6f6836a178ad280745d76d5
zotel1 <zotelsigel@gmail.com>
2024-10-21 23:55:34 -0300

    update, tenemos nuestro viewHolder preparado 7:21

--- app/src/main/java/com/zoteldev/androidfirst/todoapp/CategoriesViewHolder.kt
+++ app/src/main/java/com/zoteldev/androidfirst/todoapp/CategoriesViewHolder.kt
@@ -1,11 +1,17 @@
 package com.zoteldev.androidfirst.todoapp
 
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.zoteldev.androidfirst.R
 
 class CategoriesViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
+    private val tvCategoryName: TextView = view.findViewById(R.id.tvCategoryName)
+    private val divider:View = view.findViewById(R.id.divider)
+
     fun render(taskCategory: TaskCategory) {
 
+        tvCategoryName.text = "EJEMPLO"
     }
 }
