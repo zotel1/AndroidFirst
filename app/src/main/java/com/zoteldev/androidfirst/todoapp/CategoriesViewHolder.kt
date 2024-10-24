0efb42de77ca60f30cf988da4712f9cbc69fa61d
zotel1 <zotelsigel@gmail.com>
2024-10-23 22:22:23 -0300

    update, agregamos margen y color a nuestras categorias

--- app/src/main/java/com/zoteldev/androidfirst/todoapp/CategoriesViewHolder.kt
+++ app/src/main/java/com/zoteldev/androidfirst/todoapp/CategoriesViewHolder.kt
@@ -2,6 +2,7 @@ package com.zoteldev.androidfirst.todoapp
 
 import android.view.View
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.zoteldev.androidfirst.R
 
@@ -17,12 +18,21 @@ class CategoriesViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         when(taskCategory) {
             TaskCategory.Personal -> {
                 tvCategoryName.text = "Personal"
+                divider.setBackgroundColor(
+                    ContextCompat.getColor(divider.context, R.color.todo_personal_category)
+                )
             }
             TaskCategory.Business -> {
                 tvCategoryName.text = "Negocios"
+                divider.setBackgroundColor(
+                    ContextCompat.getColor(divider.context, R.color.todo_business_category)
+                )
             }
             TaskCategory.Other -> {
                 tvCategoryName.text = "Otros"
+                divider.setBackgroundColor(
+                    ContextCompat.getColor(divider.context, R.color.todo_other_category)
+                )
             }
         }
 
