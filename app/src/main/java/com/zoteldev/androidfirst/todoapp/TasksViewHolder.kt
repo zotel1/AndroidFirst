b681f05576631ddbd93d46ec6f33529243c6ad26
zotel1 <zotelsigel@gmail.com>
2024-10-24 23:45:27 -0300

    update, empezamos a dar funcionalidad a nuestro TaskViewHolder

--- app/src/main/java/com/zoteldev/androidfirst/todoapp/TasksViewHolder.kt
+++ app/src/main/java/com/zoteldev/androidfirst/todoapp/TasksViewHolder.kt
@@ -1,11 +1,17 @@
 package com.zoteldev.androidfirst.todoapp
 
 import android.view.View
+import android.widget.CheckBox
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.zoteldev.androidfirst.R
 
 class TasksViewHolder (view: View) : RecyclerView.ViewHolder(view){
 
-    fun render(task: Task) {
+    private val tvTask:TextView = view.findViewById(R.id.tvTask)
+    private val cbTask:CheckBox = view.findViewById(R.id.cbTask)
 
+    fun render(task: Task) {
+        tvTask.text = task.name
     }
 }
