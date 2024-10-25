d33f40b4205830a38141d81ff35ee17a4d82bb1a
zotel1 <zotelsigel@gmail.com>
2024-10-24 23:35:49 -0300

    update, copiando los valores, aunque similares al CategoriesAdapter, obtenemos nuestro TaskAdapter

--- app/src/main/java/com/zoteldev/androidfirst/todoapp/TasksAdapter.kt
+++ app/src/main/java/com/zoteldev/androidfirst/todoapp/TasksAdapter.kt
@@ -1,16 +1,19 @@
 package com.zoteldev.androidfirst.todoapp
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.zoteldev.androidfirst.R
 
 class TasksAdapter (private val  tasks:List<Task>) : RecyclerView.Adapter<TasksViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TasksViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_todo_task, parent, false)
+        return TasksViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: TasksViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.render(tasks[position])
     }
 
     override fun getItemCount() = tasks.size
