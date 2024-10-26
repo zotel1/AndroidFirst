1922575258916579d071fe3490d113f5a9c88969
zotel1 <zotelsigel@gmail.com>
2024-10-25 23:30:04 -0300

    update

--- app/src/main/java/com/zoteldev/androidfirst/todoapp/TodoActivity.kt
+++ app/src/main/java/com/zoteldev/androidfirst/todoapp/TodoActivity.kt
@@ -8,19 +8,27 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.zoteldev.androidfirst.R
+import com.zoteldev.androidfirst.todoapp.TaskCategory.*
 
 class TodoActivity : AppCompatActivity() {
 
     private val categories = listOf(
-        TaskCategory.Personal,
-        TaskCategory.Business,
-        TaskCategory.Other
+        Personal,
+        Business,
+        Other
+    )
+
+    private val tasks = mutableListOf(
+        Task("PruebaBusiness", Business),
+        Task("PruebaPersonal", Personal),
+        Task("PruebaOther", Other)
     )
 
     private lateinit var rvCategories: RecyclerView
     private lateinit var categoriesAdapter: CategoriesAdapter
 
-    private lateinit var rvTask: RecyclerView
+    private lateinit var rvTasks: RecyclerView
+    private lateinit var tasksAdapter: TasksAdapter
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,7 +48,7 @@ class TodoActivity : AppCompatActivity() {
 
     private fun initComponent() {
         rvCategories = findViewById(R.id.rvCategories)
-        rvTask = findViewById(R.id.rvTask)
+        rvTasks = findViewById(R.id.rvTask)
     }
 
     private fun initUI() {
@@ -48,6 +56,11 @@ class TodoActivity : AppCompatActivity() {
         categoriesAdapter = CategoriesAdapter(categories)
         rvCategories.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         rvCategories.adapter = categoriesAdapter
+
+        tasksAdapter = TasksAdapter(tasks)
+        rvTasks.layoutManager = LinearLayoutManager(this)
+        rvTasks.adapter = tasksAdapter
+
     }
 
 }
