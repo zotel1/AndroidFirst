5aa9c404a7c9eee9d6a7d1454e6b2ea9e8807707
zotel1 <zotelsigel@gmail.com>
2024-10-27 21:46:36 -0300

    update, funciona, pero es una mala practica nuestro check box

--- app/src/main/java/com/zoteldev/androidfirst/todoapp/TasksViewHolder.kt
+++ app/src/main/java/com/zoteldev/androidfirst/todoapp/TasksViewHolder.kt
@@ -1,9 +1,11 @@
 package com.zoteldev.androidfirst.todoapp
 
+import android.content.res.ColorStateList
 import android.graphics.Paint
 import android.view.View
 import android.widget.CheckBox
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.zoteldev.androidfirst.R
 
@@ -20,6 +22,32 @@ class TasksViewHolder (view: View) : RecyclerView.ViewHolder(view){
             tvTask.paintFlags = tvTask.paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
         }
 
+
         tvTask.text = task.name
+
+        cbTask.isChecked = task.isSelected
+
+        when(task.category){
+            TaskCategory.Business -> {
+
+                cbTask.buttonTintList = ColorStateList.valueOf(
+                    ContextCompat.getColor(cbTask.context, R.color.todo_business_category)
+                )
+            }
+            TaskCategory.Personal -> {
+
+                cbTask.buttonTintList = ColorStateList.valueOf(
+                    ContextCompat.getColor(cbTask.context, R.color.todo_personal_category)
+                )
+            }
+            TaskCategory.Other -> {
+
+                cbTask.buttonTintList = ColorStateList.valueOf(
+                    ContextCompat.getColor(cbTask.context, R.color.todo_other_category)
+                )
+            }
+        }
+
+
     }
 }
