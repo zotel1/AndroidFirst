cfeff7814fc8014332a18b2f13dd2bc60dc2fd95
zotel1 <zotelsigel@gmail.com>
2024-11-08 23:25:27 -0300

    update, comenzamos a utilizar el viewBinding

--- app/src/main/java/com/zoteldev/androidfirst/superheroapp/SuperHeroListActivity.kt
+++ app/src/main/java/com/zoteldev/androidfirst/superheroapp/SuperHeroListActivity.kt
@@ -6,16 +6,19 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.zoteldev.androidfirst.R
+import com.zoteldev.androidfirst.databinding.ActivitySuperHeroListBinding
 
 class SuperHeroListActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySuperHeroListBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_super_hero_list)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivitySuperHeroListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+
     }
 }
