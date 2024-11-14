85d9ec945f1d7774c8063effcf082dae038b61a4
zotel1 <zotelsigel@gmail.com>
2024-11-13 23:26:48 -0300

    update, añadimos nuestra primer Interface ApiService

---
+++ app/src/main/java/com/zoteldev/androidfirst/superheroapp/ApiService.kt
@@ -0,0 +1,11 @@
+package com.zoteldev.androidfirst.superheroapp
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface ApiService {
+
+    @GET("/api/acces-token/search/{name}")
+    suspend fun getSuperheroes(@Path("name") superHeroName :String):Response<JSON>
+}
