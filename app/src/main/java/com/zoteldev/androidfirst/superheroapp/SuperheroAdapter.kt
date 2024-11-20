68271540b923a5b140ad28612aaf2e8ca35ce748
zotel1 <zotelsigel@gmail.com>
2024-11-19 22:21:51 -0300

    update, SuperheroAdapter

---
+++ app/src/main/java/com/zoteldev/androidfirst/superheroapp/SuperheroAdapter.kt
@@ -0,0 +1,23 @@
+package com.zoteldev.androidfirst.superheroapp
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.zoteldev.androidfirst.R
+
+
+class SuperheroAdapter(var superheroList: List<SuperheroItemResponse> = emptyList()) :
+    RecyclerView.Adapter<SuperheroViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SuperheroViewHolder {
+        return SuperheroViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_superhero, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: SuperheroViewHolder, position: Int) {
+        viewholder.bind(superheroList[position])
+    }
+
+    override fun getItemCount() = superheroList.size
+
+}
--- app/src/main/java/com/zoteldev/androidfirst/superheroapp/SuperheroViewHolder.kt
+++ app/src/main/java/com/zoteldev/androidfirst/superheroapp/SuperheroViewHolder.kt
@@ -3,7 +3,7 @@ package com.zoteldev.androidfirst.superheroapp
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
-class SuperHeroViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+class SuperheroViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     fun bind(superHeroItemResponse: SuperHeroItemResponse) {
 
     }
