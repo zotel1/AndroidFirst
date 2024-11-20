96ce50e2b7162a81f86f6886881a577a519b5681
zotel1 <zotelsigel@gmail.com>
2024-11-20 20:48:39 -0300

    update, creamos una nueva pantalla para mostrar los detalles de los super heroes

---
+++ app/src/main/java/com/zoteldev/androidfirst/superheroapp/DetailSuperHeroActivity.kt
@@ -0,0 +1,14 @@
+package com.zoteldev.androidfirst.superheroapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.zoteldev.androidfirst.R
+
+class DetailSuperHeroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_super_hero)
+
+
+    }
+}
