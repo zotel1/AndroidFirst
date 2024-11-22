88c181f9b0e55a4a8a76d9266689f6d07cc4aae7
zotel1 <zotelsigel@gmail.com>
2024-11-22 00:18:31 -0300

    update 3:00 arrancamos nuevo proyecto

---
+++ app/src/main/java/com/zoteldev/androidfirst/settings/SettingsActivity.kt
@@ -0,0 +1,13 @@
+package com.zoteldev.androidfirst.settings
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.zoteldev.androidfirst.R
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        }
+    }
