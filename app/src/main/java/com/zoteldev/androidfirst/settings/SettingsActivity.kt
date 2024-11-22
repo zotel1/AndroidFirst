632198efadf2b5f7adad1185ffa33ea77ca07661
zotel1 <zotelsigel@gmail.com>
2024-11-22 00:37:43 -0300

    update 3:07

--- app/src/main/java/com/zoteldev/androidfirst/settings/SettingsActivity.kt
+++ app/src/main/java/com/zoteldev/androidfirst/settings/SettingsActivity.kt
@@ -3,11 +3,16 @@ package com.zoteldev.androidfirst.settings
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.zoteldev.androidfirst.R
+import com.zoteldev.androidfirst.databinding.ActivitySettingsBinding
 
 class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySettingsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         }
     }
