cb7ca15c49b31d68f342c5f34b115d71404efcdd
zotel1 <zotelsigel@gmail.com>
2024-11-24 22:34:23 -0300

    update, 3:35 coroutine

--- app/src/main/java/com/zoteldev/androidfirst/settings/SettingsActivity.kt
+++ app/src/main/java/com/zoteldev/androidfirst/settings/SettingsActivity.kt
@@ -10,6 +10,9 @@ import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import com.zoteldev.androidfirst.databinding.ActivitySettingsBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
@@ -32,6 +35,9 @@ class SettingsActivity : AppCompatActivity() {
     private fun initUI() {
         binding.rsVolume.addOnChangeListener { _, value, _ ->
             Log.i("Cris", "el valor es $value")
+            CoroutineScope(Dispatchers.IO).launch {
+                saveVolume(value.toInt())
+            }
         }
     }
 
