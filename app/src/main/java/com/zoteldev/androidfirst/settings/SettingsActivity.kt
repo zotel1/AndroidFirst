ebc93880deff3c9872f7de922c1630480dd66c8c
zotel1 <zotelsigel@gmail.com>
2024-11-24 22:21:50 -0300

    update, 3:35, update

--- app/src/main/java/com/zoteldev/androidfirst/settings/SettingsActivity.kt
+++ app/src/main/java/com/zoteldev/androidfirst/settings/SettingsActivity.kt
@@ -2,10 +2,12 @@ package com.zoteldev.androidfirst.settings
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import com.zoteldev.androidfirst.databinding.ActivitySettingsBinding
 
@@ -13,18 +15,29 @@ val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "se
 
 class SettingsActivity : AppCompatActivity() {
 
+    companion object {
+        const val VOLUME_LVL = "volume_lvl"
+    }
+
     private lateinit var binding: ActivitySettingsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initUI()
 
         }
 
-    private fun saveVolume(value: Int) {
-        dataStore.edit {
+    private fun initUI() {
+        binding.rsVolume.addOnChangeListener { _, value, _ ->
+            Log.i("Cris", "el valor es $value")
+        }
+    }
 
+    private suspend fun saveVolume(value: Int) {
+        dataStore.edit { preferences ->
+            preferences[intPreferencesKey(VOLUME_LVL)] = value
         }
     }
 
